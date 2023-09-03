633a5685b303c329867374f12ea00a4ab8d603f3
jabik <tasya16mosya@gmail.com>
2023-09-03 16:35:48 +0700

    add: styles for error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,16 +1,21 @@
-import {Link, useNavigate, useRouteError} from "react-router-dom";
+import {useNavigate, useRouteError} from "react-router-dom";
 
 function ErrorPage () {
     const error = useRouteError();
     console.error(error);
     const navigate = useNavigate();
     return(
-        <div>
-            <h1>ErrorPage</h1>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
-            <button onClick={(event) => (navigate('/'))}>Вернуться на главную</button>
+        <div className='d-flex justify-content-center p-5'>
+            <div className='flex-column w-50 align-self-center text-center'>
+                <h1>ErrorPage</h1>
+                <p>
+                    <i>{error.statusText || error.message}</i>
+                </p>
+                <button onClick={() => (navigate('/'))}
+                        className='btn btn-dark'>
+                    Вернуться на главную</button>
+            </div>
+
         </div>
     )
 
