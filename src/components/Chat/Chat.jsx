448afdfad298ecce2fd2b6c5e2cb4682f98ce8a9
jabik <tasya16mosya@gmail.com>
2023-09-10 20:32:09 +0700

    feat: show date for messages

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,44 +1,62 @@
 import {useAuth} from "../../hooks/UseAuth.js";
 import { useEffect, useRef, useState } from 'react';
 import { push, ref} from "firebase/database";
-import {db} from "../../firebase.js";
-import {Timestamp} from "firebase/firestore";
+import { db } from '../../firebase.js';
+import { Timestamp } from 'firebase/firestore';
 import Message from "./Message/Message.jsx";
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import UserCard from '../UserCard/UserCard.jsx';
 import InputEnterGroup from '../InputEnterGroup/InputEnterGroup.jsx';
-import './Chat.scss'
+import './Chat.module.scss'
+import { removeFromRecentChats, unshiftToRecentChats } from '../../store/user.slice.js';
+import NoMessagesImg from '../../../public/HappyFroggy.svg';
 
 export default function Chat() {
+    const dispatch = useDispatch();
     const selectedChat = useSelector(state => state.selectedChat);
     const currentUser = useAuth();
     const [chat, setChat] = useState(selectedChat);
     useEffect(() => {setChat(selectedChat)},[selectedChat]);
-    const messageRef  = ref(db, `messages/${chat.chatId}`)
+
 
     const handleSubmit = async (messageText) => {
-        await push(messageRef, {
+        await push(ref(db, `messages/${chat.chatId}`), {
             userId: currentUser.uid,
             text: messageText,
             timeStamp: Timestamp.fromDate(new Date()),
         })
+        dispatch(removeFromRecentChats(chat.uid));
+        dispatch(unshiftToRecentChats({ uid:chat.uid, username: chat.username }));
     }
     const bottomRef = useRef(null);
     useEffect(() => {
         bottomRef.current?.scrollIntoView({behavior: 'smooth'});
     }, [chat.messagesList]);
 
+    let prevDay = new Date(chat.messagesList[0].timeStamp.seconds * 1000);
+
  return(
      <div className="chat bg-candy">
       <UserCard user={{ id:chat.uid, username:chat.username }}/>
          <div className='chat-content'>
-             {chat.messagesList === null?
-                 <div className='h-100 d-flex justify-content-center align-items-center text-secondary'>
+             {(chat.messagesList === null || !chat.messagesList.length)?
+                 <div className='h-100 d-flex flex-column justify-content-center align-items-center text-secondary'>
+                     <img src={NoMessagesImg} alt='No messages'/>
                      <p>Сообщений пока нет...</p>
                  </div>
                  :
                  chat.messagesList.map(
-               (message, key) => {return (<Message message={message} selectedUsername={chat.username} key={key}/>)}
+               (message, key) => {
+                   let currDay = new Date(message.timeStamp.seconds * 1000);
+                   if (prevDay.toLocaleDateString() !== currDay.toLocaleDateString()
+                       || key === 0 ) {
+                       prevDay = currDay;
+                       return (<Message message={message}
+                                        day={true}
+                                        selectedUsername={chat.username}
+                                        key={key}/>)
+                   }
+                   else return (<Message message={message} day={false} selectedUsername={chat.username} key={key}/>)}
            )}
              <div ref={bottomRef} />
          </div>
--- src/components/Chat/Message/Message.jsx
+++ src/components/Chat/Message/Message.jsx
@@ -1,29 +1,38 @@
 import { useAuth } from '../../../hooks/UseAuth.js';
-import './Message.scss';
+import './Message.module.scss';
 import classNames from 'classnames';
-export default function Message({ message, selectedUsername }) {
+export default function Message({ message, day, selectedUsername }) {
 	const currentUser = useAuth();
 		let date = new Date(message.timeStamp.seconds * 1000)
-		// let myDate = date.toLocaleDateString()
+		 let myDate = date.toLocaleDateString()
 		let myTime = date.toLocaleTimeString([], {
 			hour: '2-digit',
 			minute: '2-digit',
 		})
 	return (
-
-		<div
-			className={classNames('message', message.userId===currentUser.uid? 'my-message':'not-my-message')}
-		>
-			{message.userId!==currentUser.uid && <div className='not-my-message-triangle'/>}
-				<div
-					className='message-box'
-					key={message.key}>
-					{message.userId!==currentUser.uid && <p className='username'><b>{selectedUsername}</b></p>}
-					{message.userId===currentUser.uid && <p className='username'><b>{currentUser.username}</b></p>}
-					<p>{message.text}</p>
-					<p className='time'>{myTime}</p>
+		<>
+			{day &&
+				<div  className='justify-content-center my-3' >
+				<p style={{maxWidth:'100px'}} className='
+				text-center bg-light
+				bg-opacity-50 m-auto rounded-2
+				text-dark text-opacity-50'>{myDate}</p>
 				</div>
-			{message.userId===currentUser.uid && <div className='my-message-triangle'/>}
-		</div>
+				}
+			<div
+				className={classNames('message', message.userId===currentUser.uid? 'my-message':'not-my-message')}
+				>
+					{message.userId!==currentUser.uid && <div className='not-my-message-triangle'/>}
+						<div
+							className='message-box'
+							key={message.key}>
+							{message.userId!==currentUser.uid && <p className='username'><b>{selectedUsername}</b></p>}
+							{message.userId===currentUser.uid && <p className='username'><b>{currentUser.username}</b></p>}
+							<p>{message.text}</p>
+							<p className='time'>{myTime}</p>
+						</div>
+					{message.userId===currentUser.uid && <div className='my-message-triangle'/>}
+			</div>
+		</>
 	);
 }
